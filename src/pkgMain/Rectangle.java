f1f00852d481fb26a3dbf8e0bede09a911d1060e
ash311 <aautore@udel.edu>
2017-02-14 19:11:52 -0500

    Update Rectangle.java

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -2,25 +2,13 @@ package pkgMain;
 
 public class Rectangle {
 
-	private double Length;
-	private double Width;
-	public Rectangle(double length, double width) {
-		Length = length;
-		Width = width;
-		length = 4.6;
-		width = 8.7;
-	}
-	public double getLength() {
-		return Length;
-	}
-	public double getWidth() {
-		return Width;
-	}
-	
-	public double Area()
+	public static void main (String[] args)
 	{
-		area = Length*Width;
+		double Length = 4.6;
+		double Width = 8.7;
+		double area = Length*Width;
+	
 		// TODO Implement Area() function with the correct formula
-		System.out.println("The area for the rectangle of length "+length+" and width "+width+" is "+area);
+		System.out.println("The area for the rectangle is "+area);
 	}	
 }
